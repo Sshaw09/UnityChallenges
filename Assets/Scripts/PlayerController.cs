27798664b2686d612ca6a3001628348c77987757
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2022-10-14 22:53:46 -0400

    Upload + Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float horizontalInput = 1f;
+    float verticalInput = 1f;
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+
+
     }
 }
