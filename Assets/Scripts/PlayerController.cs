b86beabc2a45159eaaf80626875ec0d4cd9f929f
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2022-10-21 12:42:19 -0400

    Shams Fun

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
     Vector3 movement;
     public Rigidbody rb;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        //crossPlatformInput = GetComponent<>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //WASD player movement
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
 
+        
     }
 
     private void FixedUpdate()
